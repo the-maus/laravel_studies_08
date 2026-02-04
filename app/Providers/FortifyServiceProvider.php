d02c91206442c1e8c0427f6d065c05a9f18de8ab
the-maus <msampaio.mail@gmail.com>
2026-02-04 14:49:29 -0300

    Setting login form view to Fortify logic

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -44,5 +44,12 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        // --------------------------------------------
+        // setting function to return login view (form)
+        // --------------------------------------------
+        Fortify::loginView(function() {
+            return view('auth.login');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo "Hello Fortify!";
-});
+})->middleware(['auth']);
